55891a16ad8b0f6c5f518e6600cdef0949d155c7
Linberg07 <63363265+Linberg07@users.noreply.github.com>
2021-06-06 17:30:46 +0300

    Add files via upload

---
+++ kurs/kurs.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include "FordFulkerson.h"
+using namespace std;
+
+int main()
+{
+    string filename;
+    filename = "in.txt";
+    FordFulkerson alg;
+    alg.inputData(filename);
+    int res = alg.MaxStream();
+    cout << "Answer:" << res;
+}
---
+++ kurs_test/kurs_test.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\kurs\FordFulkerson.h"
+#include "..\kurs\kurs.cpp"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+namespace Fordtest
+{
+	TEST_CLASS(Fordtest)
+	{
+	public:
+
+		TEST_METHOD(test1)
+		{
+			string fileName ="test1.txt";
+			FordFulkerson test;
+			test.inputData(fileName);
+			Assert::AreEqual(test.MaxStream(), 20);
+		}
+		
+	};
+}
+
